4b1c310eb0863527912fbc4de73f1340d2257532
Nikhil Singhal <niks@mit.edu>
2021-01-14 06:51:27 -0500

    Implement api

--- server/api.js
+++ server/api.js
@@ -20,6 +20,7 @@ const router = express.Router();
 
 //initialize socket
 const socketManager = require("./server-socket");
+const { uploadImagePromise, deleteImagePromise, downloadImagePromise } = require("./storageTalk");
 
 router.post("/login", auth.login);
 router.post("/logout", auth.logout);
@@ -31,6 +32,77 @@ router.get("/whoami", (req, res) => {
 
   res.send(req.user);
 });
+
+
+router.post("/uploadImage", auth.ensureLoggedIn, (req, res) => {
+  if (typeof (req.body.image) !== 'string') {
+    throw new Error("Can only handle images encoded as strings. Got type: "
+      + typeof (req.body.image));
+  }
+  User.findById(req.user._id).then(user => {
+    if (user.imageNames.length >= 3) {
+      // don't allow anyone to have more than 3 images (not race condition safe)
+      res.status(412).send({
+        message: "You can't post a new image! You already have 3!"
+      });
+    }
+    // only start uploading the image once we know we really want to, since
+    // uploading costs money! (if you do it too much)
+    return uploadImagePromise(req.body.image);
+  }).then(imageName => {
+    return User.updateOne({ _id: req.user._id },
+      { $push: { imageNames: imageName } });
+  }).then(user => {
+    res.send({}); // success!
+  }).catch(err => {
+    console.log("ERR: upload image: " + err);
+    res.status(500).send({
+      message: "error uploading",
+    });
+  })
+});
+
+router.get("/getImages", auth.ensureLoggedIn, (req, res) => {
+  User.findById(req.user._id).then(user => {
+    Promise.all(
+      user.imageNames.map(imageName => downloadImagePromise(imageName)
+        .catch(err => "Err: could not find image"))
+    ).then(images => {
+      res.send(images);
+    }).catch(err => {
+      console.log("ERR getImages this shouldn't happen");
+      res.status(500).send({
+        message: "unknown error"
+      });
+    });
+  });
+});
+
+router.post("/deleteImages", auth.ensureLoggedIn, (req, res) => {
+  User.findById(req.user._id).then(user => {
+    return Promise.all(user.imageNames.map(imageName => {
+      return Promise.all([deleteImagePromise(imageName), Promise.resolve(imageName)])
+    }));
+  }).then(successesAndNames => {
+    // get names of removed images
+    return successesAndNames.filter(
+      successAndName => successAndName[0]).map(
+        successAndName => successAndName[1]);
+  }).then((removedNames) => {
+    return User.findOneAndUpdate({ _id: req.user._id },
+      { $pullAll: { imageNames: removedNames } }); // remove those names from the document
+  }).then(user => {
+    // success!
+    res.send({});
+  }).catch(err => {
+    console.log("ERR: failed to delete image: " + err);
+    res.status(500).send()
+  });
+});
+
+
+
+
 
 router.post("/initsocket", (req, res) => {
   // do nothing if user not logged in
